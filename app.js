ddd3fc11a130a215c9758454e02408d19f1d9fc9
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-11-12 00:59:58 -0800

    loading page

--- app.js
+++ app.js
@@ -11,9 +11,31 @@
  });
 
  app.get("/blog", function(req,res){
-	res.render("blog");
+	 return res.redirect('/blog/page/0/5');
  });
 
+ app.get("/blog/page/:first/:last", function(req,res)
+ {
+	request("https://blog-8c770.firebaseio.com/post.json", function(error, response, body)
+	{
+		var posts = JSON.parse(body);
+		console.log(posts);
+
+		var first = parseInt(req.param.first);
+		var last = parseInt(req.params.last);
+		if(last > posts["total"]) {
+			last = parseInt(posts["total"]);
+		}
+		console.log("first : " + first);
+		console.log("last : " + last);
+		
+		res.render("blog", {
+			posts:posts, 
+			first:first,
+			last:last
+		});
+	});
+ });
  app.get("/blog/:post", function(req,res){
 	var post = req.params.post;
 	res.render("post" + post, {post:post});
