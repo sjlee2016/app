e420f33c9f618f1708f0b43116fff99a1f148299
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-12-06 15:07:37 -0800

    mongoose db to store comments

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
  var express = require("express");
  var request = require("request");
+ var mongoose = require("mongoose");
+ var bodyParser = require("body-parser");
  var app = express();
+
  app.set("view engine" ,"ejs");
  app.use('/public', express.static('public'));
  app.use('/css', express.static('css'));
  app.use('/vendor', express.static('vendor'));
  app.use('/images', express.static('public/images'));
  app.use('/common-js', express.static('common-js'));
+ app.use(bodyParser.urlencoded({extended:false}));
+ app.use(express.json());
+ mongoose.connect('mongodb://localhost:27017/blog');
+
+
+ var commentSchema = new mongoose.Schema({
+	postnum : Number, 
+	name : String,
+	email : String,
+	comment : String
+ })
+
+ var Comment = mongoose.model("Comment", commentSchema);
+
  app.get("/",function(req,res){
 	res.render("default");
  });
@@ -38,21 +55,45 @@
 	});
  });
  app.get("/blog/post/:postnum/", function(req,res){
-	console.log(parseInt(req.params.postnum));
-	var url = "https://blog-8c770.firebaseio.com/post/post"+parseInt(req.params.postnum) + ".json";
-	console.log(url);
+	var postid = parseInt(req.params.postnum); 
+	var url = "https://blog-8c770.firebaseio.com/post/post"+ postid + ".json";
 	request(url, function(error, response, body)
 	{
-		console.log(body);
 		var post = JSON.parse(body);
-		console.log(post);
-		res.render("post", {post:post});
+		Comment.find({ postnum: postid}, function(err,comments){
+			if(err){
+				console.log("post id = " + postid); 
+				console.log("error finding comments for post " + postid);
+			}else{
+				console.log(comments);
+				res.render("post", {post:post, postnum : postid, comments : comments });
+			}
+		});
+	});
+});
+app.post("/comment/:postnum", function(req,res){
+	var postid = req.params.postnum; 
+	Comment.create({ postnum : postid, 
+					 name: req.body.name,
+					 email: req.body.email,
+					 comment: req.body.comment
+	},function(err, newComment){
+		if(err){
+			console.log("error in creating new comment");
+		}else{
+			console.log(newComment); 
+			console.log("post id is " + postid); 
+			res.redirect("/blog/post/"+ postid); 
+		}
+
 	});
 });
 
  app.get("/resume", function(req,res){
  	res.render("resume");
 });
+
+
  app.listen(3000, process.env.VIP, function(){
 
 	console.log("server listening..");
