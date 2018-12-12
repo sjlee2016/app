01b7b051e31ac7ae3034cc85a69b4797b3914248
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-12-12 02:28:04 -0800

    styling

--- app.js
+++ app.js
@@ -12,12 +12,13 @@
  var Post = require("./models/post");
 
  var seedDB = require("./seed");
- seedDB(); 
+ // seedDB(); 
  
  var app = express(),
 	  passport = require("passport"),
 	  LocalStrategy = require("passport-local"),
-	  User = require("./models/user");
+	  User = require("./models/user"),
+	  flash = require("connect-flash");
  app.set("view engine" ,"ejs");
  app.use('/public', express.static('public'));
  app.use('/css', express.static('css'));
@@ -26,7 +27,7 @@
  app.use('/common-js', express.static('common-js'));
  app.use(bodyParser.urlencoded({extended:false}));
  app.use(express.json());
-
+ app.use(flash());
  // PASSPORT CONFIGURATION
  app.use(require("express-session")({
 	secret : "this blog is made by se jin lee",
@@ -40,6 +41,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req,res,next){
+	res.locals.login = req.flash("login");
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
 
  app.get("/",function(req,res){
 	res.render("default", { currentUser: req.user});
@@ -58,7 +66,7 @@ passport.deserializeUser(User.deserializeUser());
 	.limit(5)
 	.exec(function(err,posts){
 		if(err){
-			console.log("error finding posts");
+			redirect("back");
 		}else{
 			if(posts.length < 5) {
 				isEndPage = true;
@@ -102,7 +110,7 @@ app.post("/comment/:id", isLoggedIn, function(req,res){
 				comment.save();
 				post.comments.push(comment);
 				post.save();
-				console.log(comment);
+				req.flash("success", "코멘트 작성 완료!");
 				res.redirect("/blog/post/"+ postid); 
 			}
 		});
@@ -113,22 +121,40 @@ app.post("/comment/:id", isLoggedIn, function(req,res){
  	res.render("resume");
 });
 
-app.get("/post/:id/comment/:comment_id/edit", function(req,res){
+app.get("/post/:id/comment/:comment_id/edit", isLoggedIn, function(req,res){
 	Comment.findById(req.params.comment_id, function(err,foundComment){
 		if(err){
-			res.redirect("/");
+			req.flash("error", "자신의 코멘트만 수정 가능합니다!");
+			res.redirect("back");
 		}else{
+			req.flash("success", "코멘트 수정 완료!");
 			res.render("editComment", {id: req.params.id, comment: foundComment});
 		}
 	})
 });
 
-app.post("/post/:id/comment/:comment_id/edit", function(req,res){
+// COMMENT DESTORY 
+app.post("/post/:id/comment/:comment_id/delete", checkCommentOwnership, function(req,res){
+	Comment.findByIdAndRemove(req.params.comment_id,function(err){
+		if(err){
+			req.flash("error", "권한이 없습니다");
+			res.redirect("/blog/post/"+req.params.id);
+		}else{
+			
+			req.flash("success", "코멘트를 삭제했습니다");
+			res.redirect("/blog/post/"+req.params.id);
+		}
+	});
+});
+
+app.post("/post/:id/comment/:comment_id/edit", checkCommentOwnership, function(req,res){
 	Comment.findByIdAndUpdate(req.params.comment_id, { text: req.body.comment} , function(err,updatedComment){
 		if(err){
-			res.redirect("/");
+			req.flash("error", "자신의 코멘트만 수정 가능합니다!");
+			res.redirect("/blog/post/"+req.params.id);
 		}else{
-			console.log("updated : " + updatedComment);
+
+			req.flash("success", "코멘트 수정 완료!");
 			res.redirect("/blog/post/"+req.params.id);
 		}
 	});
@@ -145,12 +171,14 @@ app.post("/register", function(req,res){
     req.body.password
 	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if(err){
-            console.log(err);
-            return res.redirect("register");
-        }
+			req.flash("error", "이미 같은 이름을 가진 유저가 있습니다");
+            res.redirect("/register");
+        }else {
         passport.authenticate("local")(req,res,function(){
-            res.redirect("/blog");
-        });
+			req.flash("success", "가입을 축하드립니다!");
+            res.redirect("back");
+		});
+	}
     });
 });
 
@@ -158,28 +186,33 @@ app.post("/register", function(req,res){
 app.get("/login", function(req, res){
 	res.render("login");
 });
-
+app.get("/loginFailure", function(req,res){
+	req.flash("error", "로그인에 실패했습니다");
+	res.redirect("/login");
+});
 app.post("/login", passport.authenticate("local", 
    {
 	   successRedirect : "/", 
-	   failureRedirect : "/login"
+	   failureRedirect : "/loginFailure"
 	}), function(req,res){
 	req.body.username 
     req.body.password
 	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if(err){
-            console.log(err);
-            return res.redirect("register");
+			req.flash("error", "로그인에 실패했습니다");
+            res.redirect("back");
         }
         passport.authenticate("local")(req,res,function(){
-            res.redirect("/blog");
+			req.flash("success", "로그인을 했습니다");
+            res.redirect("back");
         });
     });
 });
 
 app.get("/logout", function(req,res){
 	req.logout();
-    res.redirect("/");
+	req.flash("success", "성공적이게 로그아웃 했습니다");
+    res.redirect("back");
 });
 
 app.use(function(req,res,next){
@@ -190,9 +223,30 @@ function isLoggedIn(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
 	}
-	res.redirect("/login");
+	req.flash("error", "로그인을 해야합니다.");
+	res.redirect("back");
 }
 
+function checkCommentOwnership (req,res,next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err){
+				req.flash("error", "자신의 코멘트만 수정 가능합니다.");
+				res.redirect("back");
+			}else{
+				if(foundComment.author.id.equals(req.user.id)){
+					next();
+				}else{
+					req.flash("error", "자신의 코멘트만 수정 가능합니다.");
+					res.redirect("back");
+				}
+			}
+		})
+	}else{
+		req.flash("error","로그인이 필요합니다");
+		res.redirect("back");
+	}
+}
 
  app.listen(3000, process.env.VIP, function(){
 
